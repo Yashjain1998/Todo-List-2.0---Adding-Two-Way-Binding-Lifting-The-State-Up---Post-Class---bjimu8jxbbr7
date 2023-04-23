3aa5c639ee94070bb2b607309d4e86d3b873532a
Yashjain1998 <126048815+Yashjain1998@users.noreply.github.com>
2023-04-23 14:10:43 +0530

    Update Inbox.js

--- src/components/Inbox.js
+++ src/components/Inbox.js
@@ -1,41 +1,53 @@
-import React, { useState, useRef } from "react";
+import React, { useState} from "react";
 
-const Inbox = (props) => {
- 
-
+const Inbox = ({list, append}) => {
+ const[newTask,changeTask]=useState(false);
+ const[inputval,stateval]=useState("");
+ const[inputdate,statedate]=useState('2022-09-27');
+ function listadd(event){
+  event.preventDefault();
+  const lists=[...list,
+    {
+      number: list.lenght+1,
+      title: inputval,
+      date: new Date(inputdate)
+    }];
+    changeTask(!newTask)
+  return append(lists);
+  }
   return (
     <div>
       <h3>Inbox</h3>
       {!newTask && (
-        <button className="new" onClick={} id='add-new'>
+        <button className="new" onClick={()=>changeTask(!newTask)} id='add-new'>
           +Add New
         </button>
       )}
       {newTask && (
         <form className="newtask-box">
-          <input type="text" id="title" ref={}></input>
+          <input type="text" id="title" value={inputval} onChange={event=>stateval(event.target.value)}></input>
           <div className="buttons">
-            <button className="new" id="add-list" onClick={}>
+            <button className="new" id="add-list" onClick={(el)=>listadd(el)}>
               Add Task
             </button>
-            <button className="new" onClick={}>
+            <button className="new" onClick={()=>changeTask(!newTask)}>
               Cancel
             </button>
             <input
               type="date"
-              ref={}
-              defaultValue="2022-09-27"
+              Value={inputdate}
               id="date"
+              onChange={event=>statedate(event.target.value)}
             ></input>
           </div>
         </form>
       )}
       <div id="inbox">
-        {props.list.map((list) => {
+        {list.map((list) => {
           return (
-            <div className="box" key={}>
+            <div className="box" key={list.number}>
               <div className="task">
-                {} ({})
+                {list.title} ({list.date.toLocaleDateString()})
               </div>
             </div>
           );
