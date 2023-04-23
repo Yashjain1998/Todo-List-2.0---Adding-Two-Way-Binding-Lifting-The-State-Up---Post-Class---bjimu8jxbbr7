32e8d1c2e2fbbedbf03248ad56f4fba3f61bd835
Yashjain1998 <126048815+Yashjain1998@users.noreply.github.com>
2023-04-23 14:09:37 +0530

    Update MainSection.js

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -3,21 +3,18 @@ import Inbox from "./Inbox";
 import Next7Days from "./Next7Days";
 import Today from "./Today";
 
-const list = [
-  { number: 1, title: "Let's complete this", date: new Date("9/27/2022") },
-  { number: 2, title: "Should sleep at 9pm", date: new Date("9/28/2022") },
-  { number: 3, title: "Should complete react", date: new Date("10/5/2022") },
-];
-
 const MainSection = (props) => {
+  const[list,setlist]=useState([
+                                { number: 1, title: "Let's complete this", date: new Date("9/27/2022") },
+                                { number: 2, title: "Should sleep at 9pm", date: new Date("9/28/2022") },
+                                { number: 3, title: "Should complete react", date: new Date("10/5/2022") },
+                              ]);
   
   return (
     <div className="main-section">
-      {props.active === "INBOX" && (
-        <Inbox list={} append={} />
-      )}
-      {props.active === "TODAY" && <Today list={} />}
-      {props.active === "NEXT" && <Next7Days list={} />}
+      {props.active === "INBOX" && <Inbox list={list} append={setlist}/> }
+      {props.active === "TODAY" && <Today list={list} />}
+      {props.active === "NEXT" && <Next7Days list={list} />}
     </div>
   );
 };
